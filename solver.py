5b362afe0556862db6afe4784702002d69bf2f37
Simon Siena <simonharrisonsiena@gmail.com>
2024-10-06 23:18:19 -0400

    update heuristic

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ import Cube
 
 
 def solve(cube: Cube) -> List[str]:
-
+    pass
 
 
 def score_heuristic(solved_cube: Cube, cube: Cube) -> int:
@@ -13,16 +13,21 @@ def score_heuristic(solved_cube: Cube, cube: Cube) -> int:
     for l, layer in enumerate(cube.data):
         for r, row in enumerate(layer):
             for c, tile in enumerate(row):
-                
+                score += tile_score(tile, solved_cube, l, r, c)
 
 
-def distance_to_solved_position(search_tile: cube_tile, solved_cube: Cube, tile_layer: int, tile_row: int, tile_col: int) -> int:
+def tile_score(search_tile: cube_tile, solved_cube: Cube, tile_layer: int, tile_row: int, tile_col: int) -> int:
+    score = 0
     searching = True
     for l, layer in cube.data:
         for r, row in layer_space:
             for c, tile in layer_space:
                 if tile.type == search_tile.type and set(x.color for x in tile.faces) == set(x.color for x in search_tile.faces):
                     # found
+                    for face in tile.faces:
+                        for search_face in search_tile.faces:
+                            if face.color == search_face.color and face.direction == search_face.direction:
+                                score += 1
                     searching = False
                     break
             if not searching:
@@ -31,7 +36,7 @@ def distance_to_solved_position(search_tile: cube_tile, solved_cube: Cube, tile_
             break
 
     #NOTE - Manhattan Distance / 2 is the number of turns required.
-    manhattan_distance = abs(l - tile_layer) + abs(r - tile_row) + abs(c - tile_col)
-    return manhattan_distance / 2
+    score += (abs(l - tile_layer) + abs(r - tile_row) + abs(c - tile_col)) / 2
+    return score
     
     
