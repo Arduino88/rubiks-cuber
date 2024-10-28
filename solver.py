69ea1c789792557991de8acfbc197f74b10ad5e2
Tristan Yan-Klassen <107814137+sqfewqlk@users.noreply.github.com>
2024-10-28 02:25:40 -0400

    hopefully fixed the distance formula

--- solver.py
+++ solver.py
@@ -23,24 +23,123 @@ def score_heuristic(solved_cube: Cube, cube: Cube) -> int:
 
     return scaled(score, (0, 60), 26)
 
+def opposite(face_set: set) -> set:
+    for face in face_set:
+        face.replace('UP', 'DOWN')
+        face.replace('DOWN', 'UP')
+        face.replace('RIGHT', 'LEFT')
+        face.replace('LEFT', 'RIGHT')
+        face.replace('FRONT', 'BACK')
+        face.replace('BACK', 'FRONT')
+    return face_set
+
 
 def tile_score(search_tile: cube_tile, solved_cube: Cube, tile_layer: int, tile_row: int, tile_col: int) -> int:
     score = 0
     searching = True
+    
+    piece = cube_tile.get_properties(search_tile)
+    piece = piece.replace(': (', ',')
+    piece = piece.replace(') (', ',')
+    piece_data = piece[:-1].split(',')
+    piece_type = piece_data[0][:-1] #centre, edge, etc.
+    face_orient = set()
+    
+    for i in piece_data[1:]:
+        face_orient.add(i)    
+
     for l, layer in enumerate(solved_cube.data):
         for r, row in enumerate(layer):
             for c, tile in enumerate(row):
                 if tile.type == search_tile.type and set(x.color for x in tile.faces) == set(x.color for x in search_tile.faces):
-                    # found 
+                    # found
+                    s_tile = tile.get_properties()
                     searching = False
                     break
             if not searching:
                 break
         if not searching:
             break
-
+    
+    s_piece = piece.replace(': (', ',')
+    s_piece = s_piece.replace(') (', ',')
+    s_piece_data = s_piece[:-1].split(',')
+    s_piece_type = s_piece_data[0][:-1] #centre, edge, etc.
+    s_face_orient = set()
+    
+    for i in piece_data[1:]:
+        s_face_orient.add(i)
+    
+        
     #NOTE - Manhattan Distance / 2 is the number of turns required.
+    #only true for corner pieces - edge pieces have more dependancies 
     score += (abs(l - tile_layer) + abs(r - tile_row) + abs(c - tile_col)) / 2
+    
+    #orientation fix
+    match score:
+        case 0:
+            match piece_type:
+                case "edge":
+                    #+3 if misoriented
+                    if(s_face_orient != face_orient):
+                        score += 3
+                case "corner":
+                    #+2 if misoriented
+                    if(s_face_orient != face_orient):
+                        score += 2
+                case _:
+                    pass
+        case 1:
+            match piece_type:
+                case "edge":
+                    #+1 if actually a double move away
+                    if(max(abs(l - tile_layer), abs(r - tile_row), abs(c - tile_col)) == 2):
+                        score += 1  
+                    #+1 if misaligned 
+                    aligned = False
+                    for tile_face in face_orient:
+                        if(tile_face in s_face_orient):
+                            aligned = True
+                    if(aligned == False):
+                        score += 1
+                case "corner":
+                    #+1 if misoriented
+                    aligned = False
+                    for tile_face in face_orient:
+                        if(tile_face in s_face_orient):
+                            aligned = True
+                    if(aligned == False):
+                        score += 1 
+                case _:
+                    pass
+        case 2:
+            match piece_type:
+                case "edge":
+                    #+1 if far side
+                    if((l - tile_layer) == 0 or (r - tile_row) == 0 or (c - tile_col) == 0):
+                        score += 1
+                    #+1 if opposite orientation matches
+                    aligned = True
+                    for tile_face in opposite(face_orient):
+                        if(tile_face in s_face_orient):
+                            aligned = False
+                    if(aligned == False):
+                        score += 1
+                    
+                    
+                case "corner":
+                    pass #must always be two
+                case _:
+                    pass
+        case 3:
+            match piece_type:
+                case "edge":
+                    pass #case doesnt exist lol in our stupid set up
+                case "corner":
+                    pass #must always be 3
+                case _:
+                    pass
+    
     return score
     
     
